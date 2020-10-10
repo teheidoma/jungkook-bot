1df1bd2c05149a6cd3aac088e9ea7a30b81cd6e1
teheidoma <novakovskii2002@gmail.com>
2020-10-10 09:57:53 +0000

    token and dockerfile

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
+import os
+
 settings = {
-    'token': 'NDk0ODg3MzA4ODkwNDcyNDU4.W6zxuQ.PqrtqxnOA4z8aQp-Q7Q2FjUzejE',
+    'token': os.environ['TOKEN'],
     'bot': 'КОРЕЯС',
     'id': 494887308890472458
 }
